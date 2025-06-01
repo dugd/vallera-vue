f6f859c469f626f9e9b1abe004dd7fd4ce88467f
Dugd_Nitkin <dugd.nitkin@gmail.com>
2025-06-01 21:12:03 +0300

    fix typescript; make experiments

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+let likes = ref<number>(0);
+function increaseLikes() {
+  likes.value++;
+}
+
+</script>
+
+<template>
+  <h1>Cochabamba</h1>
+  <div>Likes: <strong>{{likes}}</strong></div>
+  <div>
+    <button @click="increaseLikes">Like!</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
